57e5cb4f0619de00ad752bc9a25210409f812a9e
SergSukh <s9626265386@gmail.com>
2022-06-30 11:43:48 +0400

    &&

--- infra_project/infra_project/urls.py
+++ infra_project/infra_project/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('', include('infra_app.urls', namespace='infra_app')),
     path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls')),
+    # path('blog/', include('blog.urls')),
 ]
